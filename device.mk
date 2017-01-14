2fcce159e68e427f749238950259d6b97858f5e3
Alberto97 <albertop2197@gmail.com>
2017-01-14 13:32:26 +0000

    mido: Add IMS shim
    Change-Id: I6ea1662d2fa478a460ea107f79f75171bf390d7e

--- device.mk
+++ device.mk
@@ -190,6 +190,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
+# Libshims
+PRODUCT_PACKAGES += \
+    libshims_ims
+
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8953
