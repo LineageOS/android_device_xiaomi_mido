7e9db1239137aefbb867037be87a547cab5de0f2
TheScarastic <warabhishek@gmail.com>
2017-02-04 13:32:59 +0000

    mido: Import sec_config

--- device.mk
+++ device.mk
@@ -128,6 +128,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/msm_irqbalance.conf:system/vendor/etc/msm_irqbalance.conf
 
+# IRSC
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
+
 # Media 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
