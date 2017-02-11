bb47a5f0438503c0adff8f2614f5324d5d08b381
TheScarastic <warabhishek@gmail.com>
2017-02-11 12:15:36 +0000

    mido: Use Global macro for priv-app

--- Android.mk
+++ Android.mk
@@ -54,7 +54,7 @@ $(LOCAL_BUILT_MODULE):
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT)/priv-app/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS_PRIVILEGED)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
