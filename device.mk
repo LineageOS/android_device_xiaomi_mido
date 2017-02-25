b851a9b9a428b851130eba1a4c5e6c70b231522d
TheScarastic <warabhishek@gmail.com>
2017-02-25 13:07:06 +0000

    mido: build Snap
     * Enable HAl3
     * Add Snap overlays

--- device.mk
+++ device.mk
@@ -120,6 +120,10 @@ PRODUCT_COPY_FILES += \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
 	$(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    Snap
+
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8953 \
