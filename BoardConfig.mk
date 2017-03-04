d68df0a287ce627717b54893a092d0d9648abedc
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-03-04 23:50:30 +0100

    mido: Add board config entry for android_filesystem_config.h
    Change-Id: Ic1605de93b81298b4804c6014f5f999ba0f99184

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,6 +151,9 @@ TARGET_QCOM_NO_FM_FIRMWARE := true
 USE_DEVICE_SPECIFIC_GPS := true
 TARGET_NO_RPC := true
 
+# Filesystem
+TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(LOCAL_PATH)/android_filesystem_config.h
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_mido
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
