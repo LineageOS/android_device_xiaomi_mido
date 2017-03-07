0cd95cc2969aacaaa1e3d74d08399ec08bd7ad9b
TheScarastic <warabhishek@gmail.com>
2017-03-07 06:48:44 +0000

    Revert "mido: revert to mm fp blobs"
    This reverts commit ad8d00a8a7244f7bb530b7b1a3a0fee14aebeef0.
    
     * Issue seems to be fixed with 7.3.2 blobs

--- Android.mk
+++ Android.mk
@@ -63,6 +63,32 @@ $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 
 ALL_DEFAULT_INSTALLED_MODULES += $(IMS_SYMLINKS)
 
+GOODIX_IMAGES := \
+    goodixfp.b00 goodixfp.b01 goodixfp.b02 goodixfp.b03 \
+    goodixfp.b04 goodixfp.b05 goodixfp.b06 goodixfp.mdt
+
+GOODIX_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(GOODIX_IMAGES)))
+$(GOODIX_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Goodix firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(GOODIX_SYMLINKS)
+
+FPC_IMAGES := \
+    fpctzapp.b00 fpctzapp.b01 fpctzapp.b02 fpctzapp.b03 \
+    fpctzapp.b04 fpctzapp.b05 fpctzapp.b06 fpctzapp.mdt 
+
+FPC_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FPC_IMAGES)))
+$(FPC_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Fpc firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FPC_SYMLINKS)
+
 include device/xiaomi/mido/tftp.mk
 
 endif
