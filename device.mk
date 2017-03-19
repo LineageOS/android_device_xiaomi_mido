ab5d463afc877c3d699805311ec72cd8274c34e9
TheScarastic <warabhishek@gmail.com>
2017-03-19 07:05:22 +0000

    mido: Fix Focaltech keylayout
     * Xiaomi seems to ship dummy caf keylayout in stock rom and let
       Generic.kl handle keyevents and manages vibration natively.
     * Rename ft5x06_ts to match our touch driver.

--- device.mk
+++ device.mk
@@ -169,7 +169,7 @@ PRODUCT_COPY_FILES += \
 
 # Input
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/ft5x06_ts.kl:system/usr/keylayout/ft5x06_ts.kl \
+    $(LOCAL_PATH)/keylayout/ft5435_ts.kl:system/usr/keylayout/ft5435_ts.kl \
     $(LOCAL_PATH)/keylayout/gf3208.kl:system/usr/keylayout/gf3208.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/uinput-fpc.kl:system/usr/keylayout/uinput-fpc.kl
