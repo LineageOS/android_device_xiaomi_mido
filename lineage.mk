b371d1434383b41bb604849f133ae89513d769d7
TheScarastic <warabhishek@gmail.com>
2017-04-30 11:58:10 +0000

    mido: Use stable build fingerprint

--- lineage.mk
+++ lineage.mk
@@ -25,5 +25,5 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/7.2.9:user/release-keys" \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M 7.2.9 release-keys"
+    BUILD_FINGERPRINT="xiaomi/mido/mido:6.0.1/MMB29M/V8.2.4.0.MCFMIDL:user/release-keys" \
+    PRIVATE_BUILD_DESC="mido-user 6.0.1 MMB29M V8.2.4.0.MCFMIDL release-keys"
