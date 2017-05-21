f76b26916d8c0a11cf2e18ba4c7f6d5597efd1d9
TheScarastic <warabhishek@gmail.com>
2017-05-21 13:03:12 +0000

    mido: Use WLAN driver built into kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -210,8 +210,6 @@ BOARD_WPA_SUPPLICANT_DRIVER := NL80211
 BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
 WIFI_DRIVER_FW_PATH_AP := "ap"
 WIFI_DRIVER_FW_PATH_STA := "sta"
-WIFI_DRIVER_MODULE_NAME := "wlan"
-WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/wlan.ko"
 WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit from the proprietary version
