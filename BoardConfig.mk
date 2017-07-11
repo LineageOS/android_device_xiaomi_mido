7f820d000f8b3fdf291c990bc4e239da41023124
Martinusbe <martinusbe@gmail.com>
2017-07-11 15:15:27 +0530

    mido: Enable charging current

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -186,6 +186,9 @@ TARGET_PER_MGR_ENABLED := true
 # Power
 TARGET_POWERHAL_VARIANT := qcom
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Properties
 TARGET_SYSTEM_PROP := $(LOCAL_PATH)/system.prop
 
