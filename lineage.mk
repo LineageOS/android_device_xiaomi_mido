526149cad4a8ca459c888f07e00af4481f193c9a
Luca Stefani <luca020400@lineageos.org>
2017-08-19 13:17:46 +0200

    mido: Inherit from Lineage vendor
    Change-Id: I6d60ee5643181781ba8b386f1f16cfb59610f35c

--- lineage.mk
+++ lineage.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, device/xiaomi/mido/full_mido.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 PRODUCT_NAME := lineage_mido
 BOARD_VENDOR := Xiaomi
