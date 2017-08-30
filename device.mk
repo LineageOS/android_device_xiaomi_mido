749422bcb0ed6c3b5447743c057f6e20f63a3f5a
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-08-30 21:04:31 +0800

    mido: add seccomp policy
    Change-Id: I2dcc7b256044dcf8db997359b3c4f785daae4cab

--- device.mk
+++ device.mk
@@ -302,6 +302,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
