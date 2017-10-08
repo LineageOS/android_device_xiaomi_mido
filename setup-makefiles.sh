51f90fad43b0caa6eeda080ef5a22d74bbca3715
Rashed Abdel-Tawab <rashed@linux.com>
2017-10-08 01:04:00 +0100

    mido: Generate treble-compatible makefiles
    Change-Id: I95fb9a2f30067c25a9d2ed3a235907959befcd90

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -42,9 +42,9 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 # Copyright headers and guards
 write_headers
 
-write_makefiles "$MY_DIR"/proprietary-files.txt
+write_makefiles "$MY_DIR"/proprietary-files.txt true
 echo "" >> "$PRODUCTMK"
-write_makefiles "$MY_DIR"/proprietary-files-qc.txt
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt true
 
 # Finish
 write_footers
