4e682796228eedd486b73fe978eb6d543a69f89b
TheScarastic <warabhishek@gmail.com>
2017-10-30 18:52:00 +0100

    mido: update blobs for oreo
    Change-Id: I67a514ac21b52bfbb223b848fc464d73dbe79697

--- Android.mk
+++ Android.mk
@@ -54,7 +54,7 @@ $(LOCAL_BUILT_MODULE):
 
 IMS_LIBS := libimscamera_jni.so libimsmedia_jni.so
 
-IMS_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
+IMS_SYMLINKS := $(addprefix $(TARGET_OUT_APPS)/ims/lib/arm64/,$(notdir $(IMS_LIBS)))
 $(IMS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "IMS lib link: $@"
 	@mkdir -p $(dir $@)
