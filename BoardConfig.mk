1ab81c322618e9fdb6ef757d0f7445c2a9bd1ae1
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-10-31 01:04:40 +0530

    mido: use config.fs
    Change-Id: Iaa749c11a699624271de381e4417fae66d41d465

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,7 +168,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 TARGET_NO_RPC := true
 
 # Filesystem
-TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(DEVICE_PATH)/android_filesystem_config.h
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_mido
