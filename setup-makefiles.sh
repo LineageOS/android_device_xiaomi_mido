12d08dc7e65bd572c7a389691918a49e0bc6fef5
TheScarastic <warabhishek@gmail.com>
2017-10-30 18:52:57 +0530

    mido: Add seperate list for common qcom blobs
    Change-Id: I67a514ac21b52bfbb223b848fc464d73dbe79697

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -43,6 +43,7 @@ setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
 write_headers
 
 write_makefiles "$MY_DIR"/proprietary-files.txt
+write_makefiles "$MY_DIR"/proprietary-files-qc.txt
 
 cat << EOF >> "$ANDROIDMK"
 
