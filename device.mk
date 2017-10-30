47885157bf1a29c58253ded5b743a024a17f7a93
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-10-31 01:04:40 +0530

    mido: use config.fs
    Change-Id: Iaa749c11a699624271de381e4417fae66d41d465

--- device.mk
+++ device.mk
@@ -173,10 +173,6 @@ PRODUCT_PACKAGES += \
     FMRadio \
     libfmjni
 
-# Fs_config
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl
