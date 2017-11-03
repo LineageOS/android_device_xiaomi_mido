14f5c4843f60341ec0d814976b9d3a7938defe20
TheScarastic <warabhishek@gmail.com>
2017-11-03 14:25:54 +0530

    mido: Add initial manifest and compatibility_matrix
    Change-Id: I075388fe81751eaa1df7170bb05e3db3a79fc8d8

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -170,6 +170,10 @@ TARGET_NO_RPC := true
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
+# HIDL
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
+
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_mido
 TARGET_PLATFORM_DEVICE_BASE := /devices/soc/
