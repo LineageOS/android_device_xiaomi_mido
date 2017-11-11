83b71bca41b78093ea302bc8f7452ac90dbd1d41
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-11-11 18:59:20 +0530

    mido: Fix init build
    Change-Id: I23def1cb241a62e792fe8bd7bfbead50ebbd2f60

--- init/Android.mk
+++ init/Android.mk
@@ -22,5 +22,6 @@ LOCAL_C_INCLUDES := system/core/init
 LOCAL_MODULE := libinit_mido
 LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := init_mido.cpp
+LOCAL_STATIC_LIBRARIES := libbase
 
 include $(BUILD_STATIC_LIBRARY)
