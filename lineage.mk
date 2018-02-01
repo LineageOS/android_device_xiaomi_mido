8ca5422ca464a042f8dae55d0b78d293b4fd5d85
Bruno Martins <bgcngm@gmail.com>
2018-02-02 02:38:00 +0530

    mido: Set matching build fingerprint for system, vendor and boot images
    Change-Id: I555c15eddc6dff694285ab1b23f6f9adfd64e535

--- lineage.mk
+++ lineage.mk
@@ -25,5 +25,7 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys" \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V8.5.4.0.NCFMIED release-keys"
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V8.5.4.0.NCFMIED:user/release-keys"
