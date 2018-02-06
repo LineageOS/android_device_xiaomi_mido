7061076c2d4be9e8cc356ef598dc30629c4a195f
Rashed Abdel-Tawab <rashed@linux.com>
2018-02-06 12:58:44 +0530

    mido: Build QTI HIDL power 1.0 HAL
    This fixes 01-21 07:34:55.559   536   536 E QCOM PowerHAL: extract_stats: failed to open: /d/wlan0/power_stats Error = No such file or directory
    
    Change-Id: I929a2d3aedf3bfdca962dc34720019d83c7134e1

--- device.mk
+++ device.mk
@@ -272,8 +272,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    power.msm8953
+    android.hardware.power@1.0-service-qti
 
 # QMI
 PRODUCT_PACKAGES += \
