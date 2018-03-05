a4af55ccfdffbf761b20cb254328b8ef3df39211
deadman96385 <seanhoyt963@gmail.com>
2018-03-05 16:52:45 -0600

    mido: Add device specific overlays

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
 
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
+
 # Inherit from msm8953-common
 $(call inherit-product, device/xiaomi/msm8953-common/msm8953.mk)
 
