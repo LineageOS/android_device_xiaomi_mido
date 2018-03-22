b9a433ca7f20ebb0c6cb3d02db17932c4b5072bd
TheScarastic <warabhishek@gmail.com>
2018-03-22 17:44:15 +0530

    mido: Add missing treble flags
     * missed while making msm8953-common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,7 +40,9 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 872415232
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/enable_dt2w"
 
 # Treble
+BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 PRODUCT_FULL_TREBLE_OVERRIDE := true
+PRODUCT_COMPATIBILITY_MATRIX_LEVEL_OVERRIDE := 27
 
 # Inherit from the proprietary version
 -include vendor/xiaomi/mido/BoardConfigVendor.mk
