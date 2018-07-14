19423d5408118bf9e8a6b1cf15e85a3f8149cb7f
Albert I <krascgq@outlook.co.id>
2018-07-14 16:17:36 +0800

    mido: This device is first launched with MIUI 8.1 based Android 6.0.1
    * Inherit correct Makefile
    
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Change-Id: Ied04c25af92d193569cbabac07b8b274029d836d

--- device.mk
+++ device.mk
@@ -15,7 +15,7 @@
 #
 
 $(call inherit-product, vendor/xiaomi/mido/mido-vendor.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
