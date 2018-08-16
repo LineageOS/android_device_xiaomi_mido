64d0e25aa3bba95e82daa42e2f12d4e6308ad7cc
TheScarastic <warabhishek@gmail.com>
2018-08-16 14:52:58 +0530

    mido: Update biometrics for pie update
    Change-Id: I550ccf30860a3963bd6e1217247eaf810c411ae5

--- device.mk
+++ device.mk
@@ -31,7 +31,7 @@ PRODUCT_COPY_FILES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.0-service.xiaomi_mido
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido
 
 # Consumerir
 PRODUCT_PACKAGES += \
