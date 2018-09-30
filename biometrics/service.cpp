ad3c8d4406e06a8cee3edbee636c861f47d445ac
Dede Dindin Qudsy <xtrymind@gmail.com>
2018-10-01 04:34:52 +0700

    mido: more biometric update
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Signed-off-by: Albert I <krascgq@outlook.co.id>
    Change-Id: I2f9fd7aab1de9fda356dc8b9a9c984becc39b5ca

--- biometrics/service.cpp
+++ biometrics/service.cpp
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-#define LOG_TAG "biometrics.fingerprint@2.0-service"
+#define LOG_TAG "biometrics.fingerprint@2.1-service"
 
 #include <binder/IPCThreadState.h>
 #include <binder/IServiceManager.h>
