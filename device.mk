573b2546da16220b2895eb6f9e77613455e3c890
BealeStBluesBoy <gustavo.cargnelutti@outlook.com>
2019-04-29 20:38:27 -0300

    mido: Add fluence properties
    Change-Id: I04c9676a63a2c58d120da65106b64faeb2a924f9

--- device.mk
+++ device.mk
@@ -43,6 +43,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
+# Properties
+-include $(LOCAL_PATH)/prop.mk
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.mido.rc \
---
+++ prop.mk
@@ -0,0 +1,6 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+	persist.vendor.audio.fluence.speaker=true \
+	persist.vendor.audio.fluence.voicecall=true \
+	persist.vendor.audio.fluence.voicerec=true \
+	ro.vendor.audio.sdk.fluencetype=fluence
