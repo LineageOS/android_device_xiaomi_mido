9beb2fb3502225c3ec66c3f8b26a0454072a63b4
BealeStBluesBoy <gustavo.cargnelutti@outlook.com>
2019-05-13 20:36:15 -0300

    mido: Disable fluence for voice recording
    This is essentially the same as disabling it with
    audio_platform_info.xml but much more simple
    
    Change-Id: I583afed7fe0f5470a1707578a61aed6d52651116

--- prop.mk
+++ prop.mk
@@ -1,6 +1,6 @@
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-	persist.vendor.audio.fluence.speaker=true \
-	persist.vendor.audio.fluence.voicecall=true \
-	persist.vendor.audio.fluence.voicerec=true \
-	ro.vendor.audio.sdk.fluencetype=fluence
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    ro.vendor.audio.sdk.fluencetype=fluence
